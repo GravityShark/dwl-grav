5524b119a3ded46ae8104d3dd1cb4f8fa2dae133
Gravity <alternativegravitational@protonmail.com>
2025-12-08 08:49:49 +0800

    ipc udate

---
+++ patches/patch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+if [ $# -ne 1 ]; then
+        echo "./patch.sh <patch>"
+        exit 1
+fi
+
+base=$(basename $1 .patch)
+git switch $base || git switch -c $base
+cd $(git rev-parse --show-toplevel)
+git am -3 patches/$1
