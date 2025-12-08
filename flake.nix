1e60d381263da4b883e8065ffd2752e7b2a67617
Gravity <alternativegravitational@protonmail.com>
2025-12-08 13:35:38 +0800

    dwlgravvin

--- flake.nix
+++ flake.nix
@@ -14,26 +14,7 @@
     {
       packages.${system} =
         let
-          dwl-grav = pkgs.dwl.overrideAttrs (old: {
-            version = "0.8-dev";
-            src = self;
-            buildInputs =
-              with pkgs;
-              [
-                libinput
-                libxcb
-                libxkbcommon
-                pixman
-                wayland
-                wayland-protocols
-                wlroots_0_19
-              ]
-              ++ lib.optionals enableXWayland [
-                libX11
-                xcbutilwm
-                xwayland
-              ];
-          });
+          dwl-grav = pkgs.callPackage ./dwl.nix;
         in
         {
           dwl-grav = dwl-grav;
